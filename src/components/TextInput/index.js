6b2c2eb9a89b6fdc775b60ced472f614553cad38
yacine <lentente1958@gmail.com>
2022-10-09 21:13:19 +0100

    redefine TextInput component and consum redux words state from store

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,38 +1,41 @@
 import './index.css';
 import React,{memo,useCallback,useEffect, useMemo, useState} from "react";
+import { useSelector,useDispatch } from 'react-redux'; 
+import {AtextInsert} from "../../store/text/actionsText";
 
-function TextInput({paragraph,setText,closedBox}){
-    const ref=React.createRef();  
+function TextInput({closedBox}){ 
+    const words =useSelector(state=>{return state.words});
+    const dispatchWords=useDispatch();
+    const ref=React.createRef(); 
+    //===========================================================================================
+      
     const createText=()=> ref.current.value.split(/(\s+)/).map((x,key)=>{ 
         return { 
             word:x,
             wordState:key==0 ? true:false,
             check:null
         }
-    })  ;
-    const handler=useCallback(()=>{  
-        if(ref.current.value !=""){  let rf= createText();//console.log('ggg',rf)
-            setText( rf);
-            closedBox(false);
+    });
+    //===========================================================================================
+    const handler=()=>{  
+        if(ref.current.value !=""){  
+            let rf= createText();console.log('iii',rf);
+            dispatchWords(AtextInsert(rf));
+            closedBox(false); 
         }
-    },[]); 
+    }; 
+    //=====================================================================================
     useEffect(()=>{
-
-        paragraph !==null && 
-        //( ,ref.current.value = paragraph.map( x=>x.word+" ")) 
-            
-            //paragraph.toString(r=>r.word)
-           
-        (ref.current.value =paragraph.reduce((previous, current) => {
-             return previous+current.word 
-        },"")); 
-           
-    },[paragraph]);
+        words.length>0 &&  
+                        (ref.current.value =words.reduce((previous, current) => {
+                            return previous+current.word 
+                        },"")); 
+    },[words]);
     return <>
          <div className="layer-up area-input block-center">
               <div  className="inp-text ">
                 <textarea ref={ref}  placeholder='Insert your text'></textarea>
-                <button onClick={handler} className='buttons-ok'>Insert</button>
+                <button onClick={handler} className='buttons-ok'>Insert</button> {/*  */}
               </div>
          </div>
     </>
