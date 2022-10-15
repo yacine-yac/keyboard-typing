f9b4c88a1f4a588a64122d45676ea01c721e5ba2
yacine <lentente1958@gmail.com>
2022-10-15 19:37:52 +0100

    release boxes input text and settings

--- src/components/studio/index.js
+++ src/components/studio/index.js
@@ -3,25 +3,35 @@ import TextZone from "../textZone";
 import TextInterval from "../interval";
 import TextInput from '../TextInput';
 import { useLocation,Link} from 'react-router-dom';
-import { useState } from 'react';
-function Studio(){
+import {   useState,memo } from 'react';
+import Settingszone from '../settings';
+function Studio(){ console.log('oooo')
     const {pathname}=useLocation(); 
-    const [zoneInput,setZoneInput]=useState(false); 
+    const [zoneInput,setZoneInput]=useState(false);  
+    const [box,setBox]=useState(false);
     //====================================================================
-    const handleInputBox=()=>{
-         pathname=="/text" &&  setZoneInput(prev=> !prev)
-     }
+    const handleInputBox=()=>{  
+         pathname=="/text" && (
+            box===true && setBox(prev=>!prev)
+            ,setZoneInput(prev=> !prev)
+        )
+    }
     //====================================================================
     const renderSwitch=()=>{
         switch (pathname){ 
         case "/text": return <TextZone /> ;
         case "/interval" : return   <TextInterval /> ;
         default: return <div>error loading</div>}}
+    //=====================================================================
+    const handleSettings=()=>{  
+            zoneInput==true && setZoneInput(prev=>!prev);
+            setBox(!box);  
+        }; 
+    //===========================================================================
     return <>
     <div id="demo1" className="demo1">
             <div className="demo2">
-             { renderSwitch()}
-                 
+                { renderSwitch()}
             </div> 
             <Keyboard status={!zoneInput} />
     </div>
@@ -30,16 +40,17 @@ function Studio(){
               <div className="demo1-title center">
                 { pathname==="/interval" ?  <Link to="/text">Insert Text</Link> : <Link to="/interval" >Interval Text</Link>}
               </div> 
-          }  
-          
+          }
           <div className="demo1-buttons block-center center">
-              <button onClick={handleInputBox}  className='button active-button'></button>
-              <button   className="button"></button>
-              {(zoneInput==true) && <TextInput closedBox={setZoneInput} />} 
+               <button type='button' onClick={handleInputBox}  className='button'></button>
+               <button type='button' onClick={handleSettings}  className='button button-p active-button'  ></button>
+               {box==true &&  (<Settingszone />)} 
+               {(zoneInput==true && (<TextInput closedBox={setZoneInput} />))}
+              
           </div> 
-        
+         
    </div>
     
     </>
 }
-export default Studio;
+export default memo(Studio);
