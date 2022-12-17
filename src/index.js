15557f7a7597180dc6df8b55b806dc25ea6be41b
yacine <lentente1958@gmail.com>
2022-12-17 14:17:16 +0100

    handle react router

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
+   <HashRouter>
       <Provider store={store} >
         <App  /> 
       </Provider>
-   </BrowserRouter>
+   </HashRouter>
     
   </React.StrictMode>
 );
